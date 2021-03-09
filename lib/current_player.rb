3546e61ebb426a541a3545c5ddf4190767da950f
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-09 20:36:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-turn_count = 1
-
-counter = 0
-board.each do |turn|
-  puts "Hello #{counter}"
-  counter += 1
+def turn_count(board)
+  counter = 0
+  board.each {|turn|
+  if turn == "X" || turn == "O"
+    counter += 1
+  end
+  }
+  counter
 end
